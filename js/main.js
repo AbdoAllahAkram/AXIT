595b4be125d649a02a1aaf6e4a0ed0b13ee787a1
AbdoAllah Akram <abdoallahakram@gmail.com>
2017-06-05 01:50:24 +0200

    add scroll to section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() { 
-    
+$(document).ready(function () {
+
     //Active NiceScroll
     $("html").niceScroll({
         cursorcolor: "#ff8b38",
@@ -8,44 +8,50 @@ $(document).ready(function() {
         scrollspeed: 60,
         mousescrollstep: 50
     });
-     
-    
-    $(function() {
-   'use strict';
-    $(window).scroll(function() {
-        var navbar = $('.navbar');
-        
-        if($(window).scrollTop() >= navbar.height()) {
-            navbar.addClass('nav-scrolled');
-        } else {
-            navbar.removeClass('nav-scrolled');
-        }
+
+
+    $(function () {
+        'use strict';
+        $(window).scroll(function () {
+            var navbar = $('.navbar');
+
+            if ($(window).scrollTop() >= navbar.height()) {
+                navbar.addClass('nav-scrolled');
+            } else {
+                navbar.removeClass('nav-scrolled');
+            }
+        });
+
+        // Tab Swiching
+        $('.tab-section ul li').click(function () {
+            // Add Class Selected to active tab
+            $(this).addClass('selected').siblings().removeClass('selected');
+
+            $('.tab-section .tab-content >div').hide();
+
+            $($(this).data('class')).fadeIn();
+
+        })
     });
-    
-    // Tab Swiching
-    $('.tab-section ul li').click(function() {
-        // Add Class Selected to active tab
-        $(this).addClass('selected').siblings().removeClass('selected');
-        
-        $('.tab-section .tab-content >div').hide();
-        
-        $($(this).data('class')).fadeIn();
-        
-    })
-});
-    
+
     // Loading Spinner 
     $(window).load(function () {
-            
-            $(".loading .spinner").fadeOut(1000,
-                function () {
-                    $(".loading").fadeOut(1000,
-                        function () {
-                            $(".loading").remove();
-                        } );
-                });
 
-        });
+        $(".loading .spinner").fadeOut(1000,
+            function () {
+                $(".loading").fadeOut(1000,
+                    function () {
+                        $(".loading").remove();
+                    });
+            });
+
+    });
     
-});
+    // Smoth Scroll To Website Section
+    $('.navbar a').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('value')).offset().top - 20,
+        }, 900);
+    });
 
+});
